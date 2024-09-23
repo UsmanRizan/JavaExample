c5973827db67d3dd04c095d482840148cfeaa5e0
UsmanRizan <63894787+UsmanRizan@users.noreply.github.com>
2024-09-23 17:08:40 +0530

    Created Track list in MainActivity Class

--- app/src/main/java/com/example/javaexample/MainActivity.java
+++ app/src/main/java/com/example/javaexample/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.javaexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Track> tracks = new ArrayList<>();
+        tracks.add(new Track("Mobile","Tommy"));
+        tracks.add(new Track("Web","Brian"));
+        tracks.add(new Track("Games","Carlton"));
+
     }
 }
