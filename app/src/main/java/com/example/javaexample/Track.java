7d4c452181071f1190a6c8a0ad0386137da2ba63
UsmanRizan <63894787+UsmanRizan@users.noreply.github.com>
2024-09-23 17:01:35 +0530

    Created Track class

---
+++ app/src/main/java/com/example/javaexample/Track.java
@@ -0,0 +1,12 @@
+package com.example.javaexample;
+
+public class Track {
+    public String name;
+    public String instructor;
+
+    Track(String name,String instructor){
+        this.instructor = instructor;
+        this.name = name;
+    }
+
+}
